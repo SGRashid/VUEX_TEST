f872d63a243360dc349c3d726a9abbe1401c8551
sgabdullin <sergey.gabdullin@centre-it.com>
2020-06-25 15:31:19 +0500

    develop: Добавил верстку карточек и итерацию по массиву постов

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <div id="app">
-
+  <div id="app" class="d-flex flex-column justify-content-start align-items-center">
+    <div class="post card w-50 mb-3"
+         v-for="post in posts"
+         :key="post.id"
+    >
+      <div class="card-body">
+        <h5 class="card-title text-left">{{post.title}}</h5>
+        <hr />
+        <p class="card-text text-left">{{post.body}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
-
 export default {
     name: 'App',
     data() {
@@ -30,4 +37,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+  .post {
+    max-width: 700px !important;
+    min-width: 400px !important;
+  }
 </style>
