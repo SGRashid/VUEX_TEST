6b41232492313f339b942f5cd8859e1c5a848da7
sgabdullin <sergey.gabdullin@centre-it.com>
2020-06-25 15:17:49 +0500

    develop: удалил лишнее + добавил запрос праметров

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data() {
+      return { posts: [] };
+    },
+    async mounted() {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
+        const posts = await res.json();
+        this.posts = posts;
+    },
+    components: {}
 }
 </script>
 
