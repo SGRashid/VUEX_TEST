3a15163eea06cc78ff84fea3d664a906b2e8397b
sgabdullin <sergey.gabdullin@centre-it.com>
2020-06-26 15:17:22 +0500

    develop: some changes

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -2,17 +2,40 @@
     <form class="col-md-6">
         <div class="form-group d-flex flex-column align-items-start">
             <label for="post-title">post title</label>
-            <input type="text" id="post-title" class="form-control" />
-            <label for="post-text" class="mt-3">post text</label>
-            <textarea type="text" id="post-text" class="form-control" />
-            <button type="submit" class="btn btn-primary mt-3">Create Post</button>
+            <input type="text"
+                   id="post-title"
+                   class="form-control"
+                   v-model="title"
+            />
+            <label for="post-text"
+                   class="mt-3"
+            >
+                post text
+            </label>
+            <textarea type="text"
+                      id="post-text"
+                      class="form-control"
+                      placeholder="(^_^) write here"
+                      v-model="body"
+            />
+            <button type="submit"
+                    class="btn btn-primary mt-3"
+            >
+                Create Post
+            </button>
         </div>
     </form>
 </template>
 
 <script>
     export default {
-        name: "PostForm"
+        name: "PostForm",
+        date() {
+            return {
+                title: '',
+                body: ''
+            }
+        }
     }
 </script>
 
